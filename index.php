56584b4414867a7412e0afd138429acbd9a24ffd
ycoupannec <yohann.coupannec@gmail.com>
2016-11-30 16:13:09 +0100

    mustache + index.php
    add the library mustache.
    add contained in index.php

--- index.php
+++ index.php
@@ -12,16 +12,110 @@
 	  <title>Mini Blog</title>
 	</head>
 	<body>
-
-	
 		<header class="container-fluid">
-
+			<div class="navbar ">
+			  <div class="navbar-header col-xs-8">
+			    <a class="navbar-brand" href="#">Mini Blog</a>
+			  </div>
+		        <nav class="col-xs-4">
+		            <ul class="nav navbar-nav">
+		                <li><a href="#">Ajout article</a></li>
+		                
+		                <li class="dropdown">
+		                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Catégorie <b class="caret"></b></a>
+		                    <ul class="dropdown-menu">
+		                        <li><a href="#">Lien #2-a</a></li>
+		                        <li><a href="#">Lien #2-b</a></li>
+		                        <li><a href="#">Lien #2-c</a></li>
+		                    </ul>
+		                </li>
+		                
+		                <li class="dropdown">
+		                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Auteur <b class="caret"></b></a>
+		                    <ul class="dropdown-menu">
+		                        <li><a href="#">Lien #2-a</a></li>
+		                        <li><a href="#">Lien #2-b</a></li>
+		                        <li><a href="#">Lien #2-c</a></li>
+		                    </ul>
+		                </li>
+		            </ul>
+		        </nav>
+		    </div>
 			
 		</header>
 
 	
 		<main>
-			
+			<section class="container">
+				<article class="col-xs-12 col-md-4">
+					<div class="dateCreation">10/10/98</div>
+					<div class="sujetArticle"><h2>Titre article</h2></div>
+					<div class="row">
+						<div class="col-xs-6"><a href="">Auteur</a></div>
+
+						<div class="col-xs-6"><a href="">Catégorie</a></div>
+					</div>
+
+					
+				</article>
+				<article class="col-xs-12 col-md-4">
+					<div class="dateCreation">10/10/98</div>
+					<div class="sujetArticle"><h2>Titre article</h2></div>
+					<div class="row">
+						<div class="col-xs-6"><a href="">Auteur</a></div>
+
+						<div class="col-xs-6"><a href="">Catégorie</a></div>
+					</div>
+
+					
+				</article>
+				<article class="col-xs-12 col-md-4">
+					<div class="dateCreation">10/10/98</div>
+					<div class="sujetArticle"><h2>Titre article</h2></div>
+					<div class="row">
+						<div class="col-xs-6"><a href="">Auteur</a></div>
+
+						<div class="col-xs-6"><a href="">Catégorie</a></div>
+					</div>
+
+					
+				</article>
+				<article class="col-xs-12 col-md-4">
+					<div class="dateCreation">10/10/98</div>
+					<div class="sujetArticle"><h2>Titre article</h2></div>
+					<div class="row">
+						<div class="col-xs-6"><a href="">Auteur</a></div>
+
+						<div class="col-xs-6"><a href="">Catégorie</a></div>
+					</div>
+
+					
+				</article>
+				<article class="col-xs-12 col-md-4">
+					<div class="dateCreation">10/10/98</div>
+					<div class="sujetArticle"><h2>Titre article</h2></div>
+					<div class="row">
+						<div class="col-xs-6"><a href="">Auteur</a></div>
+
+						<div class="col-xs-6"><a href="">Catégorie</a></div>
+					</div>
+
+					
+				</article>
+				<article class="col-xs-12 col-md-4">
+					<div class="dateCreation">10/10/98</div>
+					<div class="sujetArticle"><h2>Titre article</h2></div>
+					<div class="row">
+						<div class="col-xs-6"><a href="">Auteur</a></div>
+
+						<div class="col-xs-6"><a href="">Catégorie</a></div>
+					</div>
+
+					
+				</article>
+				
+				
+			</section>
 		</main>
 
 		<footer class="container-fluid">

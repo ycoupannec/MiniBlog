749a44ad889b37cc1bcab19c9d58c92d9679e74c
ycoupannec <yohann.coupannec@gmail.com>
2016-11-30 15:30:09 +0100

    Start project
    add bootstrap.
    add index.php.

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		
+
+	  <title>Mini Blog</title>
+	</head>
+	<body>
+
+	
+		<header class="container-fluid">
+
+			
+		</header>
+
+	
+		<main>
+			
+		</main>
+
+		<footer class="container-fluid">
+		
+		</footer>
+	</body>
+	<script type="text/javascript" src="js/javas.js"></script>
+</html>

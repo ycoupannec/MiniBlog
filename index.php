104bfdd924b0d101196220d0099f9a0da333005c
victorysh <victor.a@codeur.online>
2016-12-01 12:05:32 +0100

    mise en place du style

--- index.php
+++ index.php
@@ -34,57 +34,67 @@
 				</div>
 
 
-				<article class="col-xs-12 col-md-4">
+				<article class="col-xs-12 col-md-3">
 					<div class="dateCreation">10/10/98</div>
 					<div class="sujetArticle"><h2>Titre article</h2></div>
 					<div class="row">
+                         <div class="lien text-center">
 						<div class="col-xs-6"><a href="">Auteur</a></div>
 
 						<div class="col-xs-6"><a href="">Catégorie</a></div>
+                        </div>
 					</div>
 
 
 				</article>
-				<article class="col-xs-12 col-md-4">
+				<article class="col-xs-12 col-md-3">
 					<div class="dateCreation">10/10/98</div>
 					<div class="sujetArticle"><h2>Titre article</h2></div>
 					<div class="row">
+                     <div class="lien text-center">
 						<div class="col-xs-6"><a href="">Auteur</a></div>
 
 						<div class="col-xs-6"><a href="">Catégorie</a></div>
+                        </div>
 					</div>
 
 
 				</article>
-				<article class="col-xs-12 col-md-4">
+				<article class="col-xs-12 col-md-3">
 					<div class="dateCreation">10/10/98</div>
 					<div class="sujetArticle"><h2>Titre article</h2></div>
 					<div class="row">
+                         <div class="lien text-center">
 						<div class="col-xs-6"><a href="">Auteur</a></div>
 
 						<div class="col-xs-6"><a href="">Catégorie</a></div>
+                        </div>
 					</div>
 
 
 				</article>
-				<article class="col-xs-12 col-md-4">
+				<article class="col-xs-12 col-md-3">
 					<div class="dateCreation">10/10/98</div>
 					<div class="sujetArticle"><h2>Titre article</h2></div>
 					<div class="row">
+                     <div class="lien text-center">
 						<div class="col-xs-6"><a href="">Auteur</a></div>
 
 						<div class="col-xs-6"><a href="">Catégorie</a></div>
+                        </div>
 					</div>
 
 
 				</article>
-				<article class="col-xs-12 col-md-4">
+				<article class="col-xs-12 col-md-3">
 					<div class="dateCreation">10/10/98</div>
 					<div class="sujetArticle"><h2>Titre article</h2></div>
 					<div class="row">
+                     <div class="lien text-center">
 						<div class="col-xs-6"><a href="">Auteur</a></div>
 
 						<div class="col-xs-6"><a href="">Catégorie</a></div>
+                        </div>
 					</div>
 
 

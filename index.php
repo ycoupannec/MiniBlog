a028e9d785f1c1116b1ed52831d5377968d161c8
victorysh <victor.a@codeur.online>
2016-12-02 15:28:16 +0100

    mise en place ajout d'article

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
 include ".config.php";
-/*
-$auteur=$_REQUEST("auteur");
-$categorie=$_REQUEST("categorie");*/
+$nomAuteur = $_REQUEST["nom"];
+$prenom = $_REQUEST["prenom"];
+$nom = $_REQUEST["categorie"];
+$sujet = $_REQUEST["sujet"];
+$contenu = $_REQUEST["contenu"];
+$date = $_REQUEST["date"];
+
 
 require_once("Mustache/Autoloader.php");
 Mustache_Autoloader::register();
@@ -57,6 +61,78 @@ $contained['auteur']=$sth->fetchAll(PDO::FETCH_CLASS);
 	/*require_once("Mustache/Autoloader.php");*/
 
 print_r($contained);
+// ajout ou verif d'un champs
+// ajout ou récupération de l'auteur
+$sth = $bdd->prepare('SELECT * FROM `auteur`WHERE `nom`=:nom AND `prenom`=:prenom ');
+$sth->bindParam(':nom',$nomAuteur,PDO::PARAM_STR);
+$sth->bindParam(':prenom',$prenom,PDO::PARAM_STR);
+$sth->execute();
+$res=$sth->fetch();
+$test=$sth->rowCount();
+
+
+print_r($test);
+echo "SELECT * FROM `auteur` WHERE `nom`=".$prenom." and `prenom`= ".$nomAuteur." ";
+
+
+if ($test >= 1){
+    $idAuteur=$res['id'];
+    print_r($idAuteur);
+   
+    
+}
+else{ 
+    echo "l'auteur n'existe pas ";
+    $sth = $bdd->prepare('INSERT INTO `auteur` (`id`,  `nom`, `prenom`) VALUES (NULL,  :nom, :prenom);');
+    $sth->bindParam(':nom',$nomAuteur,PDO::PARAM_STR);
+    $sth->bindParam(':prenom',$prenom,PDO::PARAM_STR);
+    $sth->execute();
+    $idAuteur=$bdd->lastInsertId();
+    print_r($idAuteur);
+    
+}
+
+//AJOUT OU RECUPERATION DE LA CATEGORIE
+
+$sth = $bdd->prepare('SELECT * FROM `categorie`WHERE `nom`=:nom');
+$sth->bindParam(':nom',$nom,PDO::PARAM_STR);
+
+$sth->execute();
+$res=$sth->fetch();
+$test=$sth->rowCount();
+
+
+print_r($test);
+echo "SELECT * FROM `auteur` WHERE `nom`=".$prenom." and `prenom`= ".$nomAuteur." ";
+
+
+if ($test >= 1){
+    $idCategorie=$res['id'];
+    print_r($idCategorie);
+   
+    
+}
+else{ 
+   
+    $sth = $bdd->prepare('INSERT INTO `categorie` (`id`,  `nom`) VALUES (NULL,  :nom);');
+    $sth->bindParam(':nom',$nom,PDO::PARAM_STR);
+ 
+    $sth->execute();
+    $idCategorie=$bdd->lastInsertId();
+   
+    
+}
+
+//AJOUT DE L'ARTICLE 
+$sth = $bdd->prepare('INSERT INTO `article`(`id`, `sujet`, `contenu`,  `categorieId`, `auteurId`) VALUES (NULL, :sujet, :contenu, :categorieId, :auteurId);');
+$sth->bindParam(':sujet',$sujet,PDO::PARAM_STR);
+$sth->bindParam(':contenu',$contenu,PDO::PARAM_STR);
+$sth->bindParam(':categorieId',$idCategorie,PDO::PARAM_INT);
+$sth->bindParam(':auteurId',$idAuteur,PDO::PARAM_INT);
+$sth->execute();
+
+
+
 $headerR= $m->render($headerR, $contained); 
 $headerR.=file_get_contents('footer.php');
 
--- modifArticle.php
+++ modifArticle.php
@@ -5,7 +5,7 @@
 
 
 		<main>
-			<form class="" action="verification.php" method="post">
+			<form class="" action="index.php" method="post">
 
 			<section class="container">
 				<article class="col-xs-12">
@@ -14,11 +14,11 @@
 
 						<label for="nom">
 						    Nom :
-						    <input class="form-control" id="nom" type="text" placeholder="nom" />
+						    <input class="form-control" id="nom" type="text" placeholder="nom" name="nom" />
 						</label>
 						<label for="prenom">
 						    Prénom :
-						    <input class="form-control" id="prenom" type="text" placeholder="prenom" />
+						    <input class="form-control" id="prenom" type="text" placeholder="prenom" name="prenom" />
 						</label>
 
 
@@ -29,7 +29,7 @@
 
 						<h2>Catégorie :</h2>
 
-						<select class="selectpicker" id="categorie">
+						<select class="selectpicker" id="categorie" name="categorie">
 						  <option>Mustard</option>
 						  <option>Ketchup</option>
 						  <option>Relish</option>
@@ -37,7 +37,7 @@
 
 						<label for="ajouter">
 						    Ajouter :
-						    <input class="form-control" id="ajouter" type="text" placeholder="ajouter" />
+						    <input class="form-control" id="ajouter" type="text" placeholder="ajouter" name="categorie" />
 						</label>
 
 
@@ -52,7 +52,7 @@
 
 						<label for="sujet">
 						    Sujet :
-						    <input class="form-control" id="sujet" type="text" placeholder="sujet" />
+						    <input class="form-control" id="sujet" type="text" placeholder="sujet" name="sujet"/>
 						</label>
 
 						<label for="contenu">Contenu :</label>
@@ -63,7 +63,7 @@
 				</article>
 
 
-					<button type="submit" name="valider">ajouter</button>
+					<button type="submit" name="valider" >ajouter</button>
 					<button type="reset">réinitialiser</button>
 			</section>
 

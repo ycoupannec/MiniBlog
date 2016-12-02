b5adc28d1d506d61467698efe57e42179b34513b
ycoupannec <yohann.coupannec@gmail.com>
2016-12-02 16:32:11 +0100

    maj index.php

--- index.php
+++ index.php
@@ -23,12 +23,6 @@ include ".config.php";
 	$param=$_REQUEST['param'];
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-	$nomAuteur = $_REQUEST["nom"];
-	$prenom = $_REQUEST["prenom"];
-	$nom = $_REQUEST["categorie"];
-	$sujet = $_REQUEST["sujet"];
-	$contenu = $_REQUEST["contenu"];
-	$date = $_REQUEST["date"];
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -87,14 +81,6 @@ switch ($fonctiAppel) {
 		$headerR.=file_get_contents('modifArticle.html');
 		$contained['articleblog']= array('id' => '','sujet' => '','contenu' => '','dateCreation' => '','categorieId' => '','auteurId' => '','dateInscri' => '','nom' => '','prenom' => '','mdp' => '','nomAuteur' => '' );
 		break;
-	case 5:
-		ajoutArticle();
-		$sth = $bdd->prepare('SELECT * , auteur.nom as nomAuteur , article.id AS articleId FROM article, auteur , categorie WHERE article.categorieId=categorie.id AND article.auteurId=auteur.id ORDER BY article.dateCreation DESC');
-		$headerR.=file_get_contents('home.html');
-		$sth->execute();
-		$contained['articleblog']=$sth->fetchAll(PDO::FETCH_CLASS);
-		# code...
-		break;
 	
 	default:
 		# code...
@@ -141,75 +127,6 @@ $headerR= $m->render($headerR, $contained);
 echo $headerR;
 
 
-function ajoutArticle(){
-	// ajout ou verif d'un champs
-	// ajout ou récupération de l'auteur
-	$sth = $bdd->prepare('SELECT * FROM `auteur`WHERE `nom`=:nom AND `prenom`=:prenom ');
-	$sth->bindParam(':nom',$nomAuteur,PDO::PARAM_STR);
-	$sth->bindParam(':prenom',$prenom,PDO::PARAM_STR);
-	$sth->execute();
-	$res=$sth->fetch();
-	$test=$sth->rowCount();
-
-	echo "SELECT * FROM `auteur` WHERE `nom`=".$prenom." and `prenom`= ".$nomAuteur." ";
-
-
-	if ($test >= 1){
-	    $idAuteur=$res['id'];
-
-	    
-	}
-	else{ 
-	    echo "l'auteur n'existe pas ";
-	    $sth = $bdd->prepare('INSERT INTO `auteur` (`id`,  `nom`, `prenom`) VALUES (NULL,  :nom, :prenom);');
-	    $sth->bindParam(':nom',$nomAuteur,PDO::PARAM_STR);
-	    $sth->bindParam(':prenom',$prenom,PDO::PARAM_STR);
-	    $sth->execute();
-	    $idAuteur=$bdd->lastInsertId();
-
-	    
-	}
-
-	//AJOUT OU RECUPERATION DE LA CATEGORIE
-
-	$sth = $bdd->prepare('SELECT * FROM `categorie`WHERE `nom`=:nom');
-	$sth->bindParam(':nom',$nom,PDO::PARAM_STR);
-
-	$sth->execute();
-	$res=$sth->fetch();
-	$test=$sth->rowCount();
-
-
-	print_r($test);
-	echo "SELECT * FROM `auteur` WHERE `nom`=".$prenom." and `prenom`= ".$nomAuteur." ";
-
-
-	if ($test >= 1){
-	    $idCategorie=$res['id'];
-
-	   
-	    
-	}
-	else{ 
-	   
-	    $sth = $bdd->prepare('INSERT INTO `categorie` (`id`,  `nom`) VALUES (NULL,  :nom);');
-	    $sth->bindParam(':nom',$nom,PDO::PARAM_STR);
-	 
-	    $sth->execute();
-	    $idCategorie=$bdd->lastInsertId();
-	   
-	    
-	}
-
-	//AJOUT DE L'ARTICLE 
-	$sth = $bdd->prepare('INSERT INTO `article`(`id`, `sujet`, `contenu`,  `categorieId`, `auteurId`) VALUES (NULL, :sujet, :contenu, :categorieId, :auteurId);');
-	$sth->bindParam(':sujet',$sujet,PDO::PARAM_STR);
-	$sth->bindParam(':contenu',$contenu,PDO::PARAM_STR);
-	$sth->bindParam(':categorieId',$idCategorie,PDO::PARAM_INT);
-	$sth->bindParam(':auteurId',$idAuteur,PDO::PARAM_INT);
-	$sth->execute();
-
-}
 
 ?>
 

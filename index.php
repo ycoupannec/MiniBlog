c558bdc2ec1f8a1db3d9a26cecfcc70be1edec81
ycoupannec <yohann.coupannec@gmail.com>
2016-12-05 13:57:42 +0100

    ajout commentaire

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ include ".config.php";
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 /*	$fonctiAppel : est une variable qui contient un entier. Elle permettra d'appeler la fonction demandé.
-			- 0 : fonction openDoc()
-			- 1 : fonction backDoc()
 
 
 
@@ -47,17 +45,23 @@ catch (Exception $e)
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+ /*Selon l'action que l'on vroudrais réaliser lors de l'appel de la page index. On passe 2 parametres. Le premier($fonctiAppel) sert à définir quel action on va faire, le second ($param) sert plus à passer le parametres que l'on veux chercher. Comme les ID qui nous permettrons de filtrer.*/
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 switch ($fonctiAppel) {
+	/*Là on vas filtrer par cas de $fonctiAppel. */
 	case 0:
-		/*ajoutArticle();*/
+		/*si il est égal à 0. on juste afficher tout les articles grace à la requete ci-dessou*/
 		$sth = $bdd->prepare('SELECT * , auteur.nom as nomAuteur , article.id AS articleId FROM article, auteur , categorie WHERE article.categorieId=categorie.id AND article.auteurId=auteur.id ORDER BY article.dateCreation DESC');
+		/*On ajoute la partie home.php à la page que l'on vas afficher.*/
 		$headerR.=file_get_contents('home.html');
+		/*On execute la requete.*/
 		$sth->execute();
+		/*On découpe se que nous rend la requete. On l'ajoute à notre tableau qui vas contenir toute les modifications que l'on veux apporter à la page en précisant qu'elle information on va vouloir changer.*/
 		$contained['articleblog']=$sth->fetchAll(PDO::FETCH_CLASS);
 		# code...
 		break;
 	case 1:
-		
+		// Ici nous voulans afficher que les catégories avec l'ID qu'on à passer en parametre dans $param.
 		$sth = $bdd->prepare('SELECT * , auteur.nom as nomAuteur , article.id AS articleId FROM article, auteur , categorie WHERE article.categorieId=categorie.id AND article.auteurId=auteur.id AND categorie.id =:var ORDER BY article.dateCreation DESC');
 		$sth->bindParam(':var',$param, PDO::PARAM_INT);
 		$headerR.=file_get_contents('home.html');
@@ -65,6 +69,7 @@ switch ($fonctiAppel) {
 		$contained['articleblog']=$sth->fetchAll(PDO::FETCH_CLASS);
 		break;
 	case 2:
+		/*Ici nous voulans afficher que les auteurs avec l'ID qu'on à passer en parametre dans $param.*/
 		$sth = $bdd->prepare('SELECT * , auteur.nom as nomAuteur , article.id AS articleId FROM article, auteur , categorie WHERE article.categorieId=categorie.id AND article.auteurId=auteur.id AND article.auteurId =:var ORDER BY article.dateCreation DESC');
 		$sth->bindParam(':var',$param, PDO::PARAM_INT);
 		$headerR.=file_get_contents('home.html');
@@ -72,6 +77,7 @@ switch ($fonctiAppel) {
 		$contained['articleblog']=$sth->fetchAll(PDO::FETCH_CLASS);
 		break;
 	case 3:
+		/*Ici nous voulans afficher que l'article avec l'ID qu'on à passer en parametre dans $param. pour le modifier par la suite.*/
 
 		$sth = $bdd->prepare('SELECT * , auteur.nom as nomAuteur , article.id AS articleId FROM article, auteur , categorie WHERE article.categorieId=categorie.id AND article.auteurId=auteur.id AND article.id =:var ORDER BY article.dateCreation DESC');
 		$sth->bindParam(':var',$param, PDO::PARAM_INT);
@@ -81,6 +87,7 @@ switch ($fonctiAppel) {
 
 		break;
 	case 4:
+		/*Ici nous voulans afficher aucune données car nous somme dans l'ajout d'article.*/
 		$headerR.=file_get_contents('modifArticle.html');
 		$contained['articleblog']= array('id' => '','sujet' => '','contenu' => '','dateCreation' => '','categorieId' => '','auteurId' => '','dateInscri' => '','nom' => '','prenom' => '','mdp' => '','nomAuteur' => '' );
 		break;
@@ -98,6 +105,9 @@ switch ($fonctiAppel) {
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*On va à present chercher les catégories puis les auteurs pour la bar du menu.*/
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 
 
@@ -105,28 +115,26 @@ $sth = $bdd->prepare('SELECT * FROM `categorie`');
 $sth->execute();
 
 
+/*Comme précedemment on ajoute nos valeur de catégorie dans notre tableau de modification avec l'attribut "categorie".*/
 $contained['categorie'] = $sth->fetchAll(PDO::FETCH_CLASS);
 
-
-	/*require_once("Mustache/Autoloader.php");*/
-$m = new Mustache_Engine;
-
-
-//$headerR= $m->render($headerR, array('categorie' => $contained)); 
 
 $sth = $bdd->prepare('SELECT * FROM `auteur`');
 $sth->execute();
 
-
+/*De même pour les auteurs.*/
 $contained['auteur']=$sth->fetchAll(PDO::FETCH_CLASS);
 
-	/*require_once("Mustache/Autoloader.php");*/
 
-/*print_r($contained);*/
+/*On ajoute notre footer à la page que l'on va afficher.*/
 $headerR.=file_get_contents('footer.html');
+/*On instancie l'objet mustache.*/
+$m = new Mustache_Engine;
+/*On pace le contenu de la page à modifier et le tableau qui contient les modifications à apporter grace à mustache il va changer nos valeur.*/
+/*Puis on les récupere dans notre variable qui contient le contenu HTML à afficher.*/
 $headerR= $m->render($headerR, $contained); 
 
-
+/*Enfin on affiche notre contenu de la variable $headerR qui contient notre page complète en HTML.*/
 echo $headerR;
 
 
